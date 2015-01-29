5e261c4d5d8d3a9d0a31dfb3fcbb6c5fb901ef74
joselinoneto <joselinoneto@gmail.com>
2015-01-29 10:41:55 -0200

    Initial Commit

---
+++ btc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  btc
+//
+//  Created by Jose Lino Neto on 1/29/15.
+//  Copyright (c) 2015 Construtor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ btc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  btc
+//
+//  Created by Jose Lino Neto on 1/29/15.
+//  Copyright (c) 2015 Construtor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
